a989b8a982d3c3d51684133eff98fc93791f4ccb
santiguillengar <SantiGuillenGar@MacBook-Pro-de-Santi-5.local>
2017-10-07 01:51:11 +0100

    app now displays mmap through lyricMapUI

--- app/src/main/java/s1546270/songle/LyricMapUI.java
+++ app/src/main/java/s1546270/songle/LyricMapUI.java
@@ -1,18 +1,44 @@
 package s1546270.songle;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.LocationListener;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LyricMapUI extends FragmentActivity implements OnMapReadyCallback {
+public class LyricMapUI
+        extends FragmentActivity
+        implements OnMapReadyCallback,
+            GoogleApiClient.ConnectionCallbacks,
+            GoogleApiClient.OnConnectionFailedListener,
+            LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted = false;
+    private Location mLastLocation;
+    private static final String TAG = "LyricMapUI";
+
+    // (!) MINE FROM IMPROVISNG BLOCK
+    //private Marker mCurrLocationMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +48,17 @@ public class LyricMapUI extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+        // Create an instance of GoogleAPIClient
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +74,97 @@ public class LyricMapUI extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+        /*
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+        try {
+            // Visualise current position with a small blue circle
+            mMap.setMyLocationEnabled(true);
+        }
+        catch (SecurityException se){
+            System.out.println("Security exception thrown [onMapReady]");
+        }
+
+        //Add "My location" button to the UI
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        /*
+        // (!) FOLLOWING BLOCK IS ME IMPROVISING
+        LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    protected void createLocationRequest() {
+        // Set the parameters for the location request
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); // preferably every 5 seconds
+        mLocationRequest.setFastestInterval(1000); // at most every second
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Can we access the user's current location?
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try { createLocationRequest(); }
+        catch (java.lang.IllegalStateException ise) {
+            System.out.println("IllegalStateException thrown [onConnected]");
+        }
+
+        // Can we access the user's current location?
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)  {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location current) {
+        System.out.println(" [onLocationChanged] Lat/long now (" +
+        String.valueOf(current.getLatitude()) + "," +
+        String.valueOf(current.getLongitude()) +")");
+        //Do something with current location
+
+        LatLng latLng = new LatLng(current.getLatitude(), current.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
+    }
+
+    @Override
+    public void onConnectionSuspended(int flag) {
+        System.out.println(" >>>> onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle the
+        // failure silently
+        System.out.println(" >>>> onConnectionFailed");
     }
 }
